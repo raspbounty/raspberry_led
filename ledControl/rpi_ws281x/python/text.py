16e50e6295d39522523063139d0f2222bbdd5637
Luca Frenk <luca.frenk@rwth-aachen.de>
2018-07-02 11:56:16 +0200

    replaced alphabet list with a dictionary

--- ledControl/rpi_ws281x/python/text.py
+++ ledControl/rpi_ws281x/python/text.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
-# NeoPixel library strandtest example
-# Author: Tony DiCola (tony@tonydicola.com)
-#
-# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
-# various animations on a strip of NeoPixels.
 
 import time
 from neopixel import *
-import argparse
 
 # LED strip configuration:
 LED_COUNT      = 64      # Number of LED pixels.
@@ -19,6 +13,21 @@ LED_BRIGHTNESS = 10     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
+alphabet = {'A':[42, 43, 44, 45, 46, 33, 36, 25, 28, 18, 19, 20, 21, 22],
+    'B':[41, 42, 43, 44, 45, 46, 33, 35, 38, 25, 27, 30, 18, 20, 21],
+    'C':[42, 43, 44, 45, 33, 38, 25, 30, 18, 21],
+    'D':[41, 42, 43, 44, 45, 46, 33, 38, 25, 30, 18, 19, 20, 21],
+    'E':[41, 42, 43, 44, 45, 46, 33, 35, 38, 25, 27, 30, 17, 22],
+    'F':[41, 42, 43, 44, 45, 46, 33, 35, 25, 27, 17],
+    'G':[42, 43, 44, 45, 33, 38, 25, 28, 30, 18, 20, 21],
+    'H':[41, 42, 43, 44, 45, 46, 35, 27, 17, 18, 19, 20, 21, 22],
+    'I':[41, 46, 33, 34, 35, 36, 37, 38, 25, 26, 27, 28, 29, 30, 17, 22],
+    'J':[44, 45, 38, 30, 17, 18, 19, 20, 21],
+    'K':[41, 42, 43, 44, 45, 46, 35, 26, 28, 17, 21, 22],
+    'L':[41, 42, 43, 44, 45, 46, 38, 30, 22],
+    'M':[49, 50, 51, 52, 53, 54, 42, 35, 27, 18, 9, 10, 11, 12, 13, 14],
+    'N':[41, 42, 43, 44, 45, 46, 35, 28, 17, 18, 19, 20, 21, 22],
+    'O':[42, 43, 44, 45, 33, 38, 25, 30, 18, 19, 20, 21]}
 
 
 # Define functions which animate LEDs in various ways.
@@ -36,24 +45,49 @@ def draw(strip, color, points, wait_ms=50):
             time.sleep(wait_ms/1000)
     strip.show()
 
+def stringToList(text):
+    output = []
+    #toDo: implement also lower case letter and remove this
+    text = text.upper()
+    for letter in text:
+        if letter in alphabet:
+            output.append(alphabet[letter])
+        else:
+            #append questionmark
+            print("unknown symbol {}".format(letter))
+
 def write(strip, color, text, wait_ms=50):
+    textList = stringToList(text)
     formattedText = []
     flatText = []
     counter = 0
-    for letter in text:
+    for letter in textList:
         formattedText.append([x-(64*counter) for x in letter])
         counter = counter + 1
-    formattedText.sort()
-    formattedText = formattedText[::-1]
+    
+    #flatten the text lists to one list
     flatText = [item for sublist in formattedText for item in sublist]
+    #sort the text list
+    formattedText.sort()
+    #reverse text list
+    formattedText = formattedText[::-1]    
 
+    
     while flatText[-1] < 63:
+        #clear screen
         for i in range(strip.numPixels()):
             strip.setPixelColor(i, Color(0,0,0))
+        #draw the current text list
         draw(strip, color, flatText, 0)
         strip.show()
         time.sleep(250/1000.0)
+        #move text 1 coloumn to the left
         flatText = [x+8 for x in flatText]
+    
+    #clear screen
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, Color(0,0,0))
+    strip.show()
 
 
 # Main program logic follows:
@@ -65,24 +99,7 @@ if __name__ == '__main__':
 
     wait_ms = 50
     
-    alphabet = [[42, 43, 44, 45, 46, 33, 36, 25, 28, 18, 19, 20, 21, 22],
-    [41, 42, 43, 44, 45, 46, 33, 35, 38, 25, 27, 30, 18, 20, 21],
-    [42, 43, 44, 45, 33, 38, 25, 30, 18, 21],
-    [41, 42, 43, 44, 45, 46, 33, 38, 25, 30, 18, 19, 20, 21],
-    [41, 42, 43, 44, 45, 46, 33, 35, 38, 25, 27, 30, 17, 22],
-    [41, 42, 43, 44, 45, 46, 33, 35, 25, 27, 17],
-    [42, 43, 44, 45, 33, 38, 25, 28, 30, 18, 20, 21],
-    [41, 42, 43, 44, 45, 46, 35, 27, 17, 18, 19, 20, 21, 22],
-    [41, 46, 33, 34, 35, 36, 37, 38, 25, 26, 27, 28, 29, 30, 17, 22],
-    [44, 45, 38, 30, 17, 18, 19, 20, 21],
-    [41, 42, 43, 44, 45, 46, 35, 26, 28, 17, 21, 22],
-    [41, 42, 43, 44, 45, 46, 38, 30, 22],
-    [49, 50, 51, 52, 53, 54, 42, 35, 27, 18, 9, 10, 11, 12, 13, 14],
-    [41, 42, 43, 44, 45, 46, 35, 28, 17, 18, 19, 20, 21, 22],
-    [42, 43, 44, 45, 33, 38, 25, 30, 18, 19, 20, 21]]
-    text = [alphabet[11], alphabet[8], alphabet[11], alphabet[14]]
-
     try:
-        write(strip, Color(0,0,255), text)
+        write(strip, Color(0,0,255), "LILO")
     except KeyboardInterrupt:
     	colorWipe(strip, Color(0, 0, 0), 10)
