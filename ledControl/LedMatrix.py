ed2a9ee4a319c11b83a96d2f9fce14336718724b
Luca Frenk <luca.frenk@rwth-aachen.de>
2018-07-10 23:32:13 +0200

    main method added

--- ledControl/LedMatrix.py
+++ ledControl/LedMatrix.py
@@ -1,6 +1,7 @@
 import time
 from neopixel import *
 
+
 class LedMatrix:
     # LED strip configuration:
     LED_COUNT      = 64      # Number of LED pixels.
@@ -12,35 +13,35 @@ class LedMatrix:
     LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
     LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
     
-    alphabet = {'A':[42, 43, 44, 45, 46, 33, 36, 25, 28, 18, 19, 20, 21, 22],
-                'B':[41, 42, 43, 44, 45, 46, 33, 35, 38, 25, 27, 30, 18, 20, 21],
-                'C':[42, 43, 44, 45, 33, 38, 25, 30, 18, 21],
-                'D':[41, 42, 43, 44, 45, 46, 33, 38, 25, 30, 18, 19, 20, 21],
-                'E':[41, 42, 43, 44, 45, 46, 33, 35, 38, 25, 27, 30, 17, 22],
-                'F':[41, 42, 43, 44, 45, 46, 33, 35, 25, 27, 17],
-                'G':[42, 43, 44, 45, 33, 38, 25, 28, 30, 18, 20, 21],
-                'H':[41, 42, 43, 44, 45, 46, 35, 27, 17, 18, 19, 20, 21, 22],
-                'I':[41, 33, 25, 34, 35, 36, 37, 38, 46, 30],
-                'J':[44, 45, 38, 30, 17, 18, 19, 20, 21],
-                'K':[41, 42, 43, 44, 45, 46, 35, 26, 28, 17, 21, 22],
-                'L':[41, 42, 43, 44, 45, 46, 38, 30, 22],
-                'M':[49, 50, 51, 52, 53, 54, 42, 35, 27, 18, 9, 10, 11, 12, 13, 14],
-                'N':[41, 42, 43, 44, 45, 46, 35, 28, 17, 18, 19, 20, 21, 22],
-                'O':[42, 43, 44, 45, 33, 38, 25, 30, 18, 19, 20, 21],
-                'P':[41, 33, 25, 18, 19, 28, 36, 44, 43, 42, 45, 46],
-                'Q':[42, 33, 25, 18, 19, 20, 21, 45, 44, 43, 38, 30, 22, 14],
-                'R':[46, 45, 44, 22, 21, 28, 36, 19, 18, 25, 33, 41, 42, 43],
-                'S':[38, 30, 21, 45, 28, 35, 42, 33, 25, 18],
-                'T':[41, 33, 25, 17, 49, 34, 35, 36, 37, 38],
-                'U':[46, 38, 30, 22, 21, 20, 19, 18, 17, 41, 42, 43, 44, 45],
-                'V':[38, 30, 21, 45, 20, 19, 18, 17, 41, 42, 43, 44],
-                'W':[46, 30, 37, 36, 53, 52, 51, 50, 18, 19, 20, 21, 17, 49],
-                'X':[41, 42, 17, 18, 27, 35, 44, 45, 46, 20, 21, 22],
-                'Y':[49, 50, 17, 18, 43, 27, 36, 37, 38],
-                'Z':[46, 38, 30, 22, 45, 36, 27, 18, 17, 25, 33, 41]}
+    alphabet = {'A': [42, 43, 44, 45, 46, 33, 36, 25, 28, 18, 19, 20, 21, 22],
+                'B': [41, 42, 43, 44, 45, 46, 33, 35, 38, 25, 27, 30, 18, 20, 21],
+                'C': [42, 43, 44, 45, 33, 38, 25, 30, 18, 21],
+                'D': [41, 42, 43, 44, 45, 46, 33, 38, 25, 30, 18, 19, 20, 21],
+                'E': [41, 42, 43, 44, 45, 46, 33, 35, 38, 25, 27, 30, 17, 22],
+                'F': [41, 42, 43, 44, 45, 46, 33, 35, 25, 27, 17],
+                'G': [42, 43, 44, 45, 33, 38, 25, 28, 30, 18, 20, 21],
+                'H': [41, 42, 43, 44, 45, 46, 35, 27, 17, 18, 19, 20, 21, 22],
+                'I': [41, 33, 25, 34, 35, 36, 37, 38, 46, 30],
+                'J': [44, 45, 38, 30, 17, 18, 19, 20, 21],
+                'K': [41, 42, 43, 44, 45, 46, 35, 26, 28, 17, 21, 22],
+                'L': [41, 42, 43, 44, 45, 46, 38, 30, 22],
+                'M': [49, 50, 51, 52, 53, 54, 42, 35, 27, 18, 9, 10, 11, 12, 13, 14],
+                'N': [41, 42, 43, 44, 45, 46, 35, 28, 17, 18, 19, 20, 21, 22],
+                'O': [42, 43, 44, 45, 33, 38, 25, 30, 18, 19, 20, 21],
+                'P': [41, 33, 25, 18, 19, 28, 36, 44, 43, 42, 45, 46],
+                'Q': [42, 33, 25, 18, 19, 20, 21, 45, 44, 43, 38, 30, 22, 14],
+                'R': [46, 45, 44, 22, 21, 28, 36, 19, 18, 25, 33, 41, 42, 43],
+                'S': [38, 30, 21, 45, 28, 35, 42, 33, 25, 18],
+                'T': [41, 33, 25, 17, 49, 34, 35, 36, 37, 38],
+                'U': [46, 38, 30, 22, 21, 20, 19, 18, 17, 41, 42, 43, 44, 45],
+                'V': [38, 30, 21, 45, 20, 19, 18, 17, 41, 42, 43, 44],
+                'W': [46, 30, 37, 36, 53, 52, 51, 50, 18, 19, 20, 21, 17, 49],
+                'X': [41, 42, 17, 18, 27, 35, 44, 45, 46, 20, 21, 22],
+                'Y': [49, 50, 17, 18, 43, 27, 36, 37, 38],
+                'Z': [46, 38, 30, 22, 45, 36, 27, 18, 17, 25, 33, 41]}
     
     def __init__(self, iconSet=0):
-        #if iconset=0:
+        # if iconset=0:
         cloudIds = [1, 2, 8, 9, 10, 11, 16, 17, 18, 19, 24, 25, 26,27, 32, 33, 
                     34, 35, 40, 41, 42, 43, 48, 49, 50,51, 57, 58]
         showerRainIds = [21, 23, 37, 39, 53, 55]
@@ -48,7 +49,7 @@ class LedMatrix:
         clearSkyIds = [0, 3, 7, 9, 14, 19, 20, 26, 27, 28, 29, 31, 32, 34, 35, 
                        36, 37, 43, 44, 49, 54, 56, 60, 63]
         
-        #icons with structure: [[color,ledIDs],[color,ledIDs],...]
+        # icons with structure: [[color,ledIDs],[color,ledIDs],...]
         self.icons = {'cloud':      [[Color(255, 255, 255), cloudIds]],
                       'shower_rain':[[Color(255, 255, 255), cloudIds],
                                      [Color(0, 0, 255), showerRainIds]],
@@ -61,7 +62,7 @@ class LedMatrix:
         self.strip.begin()
         
     def colorWipe(self, color=Color(0, 0, 0), wait_ms=50):
-        #Wipe color across display a pixel at a time.
+        # Wipe color across display a pixel at a time.
         for i in range(self.strip.numPixels()):
             self.strip.setPixelColor(i, color)
             self.strip.show()
@@ -85,13 +86,13 @@ class LedMatrix:
 
     def stringToList(self, text):
         output = []
-        #toDo: implement also lower case letter and remove this
+        # toDo: implement also lower case letter and remove this
         text = text.upper()
         for letter in text:
             if letter in self.alphabet:
                 output.append(self.alphabet[letter])
             else:
-                #append questionmark
+                # append questionmark
                 print("unknown symbol {}".format(letter))
         return output
 
@@ -104,26 +105,33 @@ class LedMatrix:
             formattedText.append([x-(64*counter) for x in letter])
             counter = counter + 1
         
-        #flatten the text lists to one list
+        # flatten the text lists to one list
         flatText = [item for sublist in formattedText for item in sublist]
-        #sort the text list
+        # sort the text list
         flatText.sort()
-        #reverse text list
-        flatText = flatText[::-1]    
+        # reverse text list
+        flatText = flatText[::-1]
 
         
         while flatText[-1] < 63:
-            #clear screen
+            # clear screen
             for i in range(self.strip.numPixels()):
                 self.strip.setPixelColor(i, Color(0,0,0))
-            #draw the current text list
+            # draw the current text list
             draw(color, flatText, 0)
             self.strip.show()
             time.sleep(250/1000.0)
-            #move text 1 coloumn to the left
+            # move text 1 coloumn to the left
             flatText = [x+8 for x in flatText]
         
-        #clear screen
+        # clear screen
         for i in range(self.strip.numPixels()):
             self.strip.setPixelColor(i, Color(0,0,0))
         self.strip.show()
+# a short Demo
+if __name__ == "__main__":
+    matrix = LedMatrix()
+    
+    matrix.write(Color(0,0,255), 'Hello')
+    
+    matrix.showIcon('sun')
