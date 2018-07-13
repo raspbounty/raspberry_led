f573ade74897d7512f39b9966b8815aa20c79fca
Luca Frenk <luca.frenk@rwth-aachen.de>
2018-07-13 15:20:32 +0200

    button push event added to weatherLed

--- ledControl/weatherLED.py
+++ ledControl/weatherLED.py
@@ -9,6 +9,7 @@ from datetime import timedelta
 import urllib
 import sys, os
 from LedMatrix import LedMatrix
+import RPi.GPIO as GPIO
 
 countryID = '6553047'
 apiKey = '2a6b0bc577fb4cbfc7a48b69afcc3eec'
@@ -72,7 +73,16 @@ def getWeatherUpdate():
             '''
     return output
 
+def button_callback(channel):
+    print("Pressed")
+    
 if __name__ == '__main__':
+    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 23 to be an input pin and set initial value to be pulled low (off)
+    
+    GPIO.add_event_detect(23,GPIO.FALLING,callback=button_callback, bouncetime = 300) # Setup event on pin 23 rising edge       
+    
     matrix = LedMatrix(1)
     currWeatherIcon = 'unset'
     try:
@@ -82,3 +92,4 @@ if __name__ == '__main__':
             time.sleep(1800)
     except KeyboardInterrupt:
         matrix.colorWipe()
+        GPIO.cleanup()
